b90671f38cf1a724786f62542c03bcc754d1bea2
EpicHigh <prasit_tongpradit@windowslive.com>
2020-06-29 01:40:37 +0700

    🥅Catching errors.

--- src/store/actions/charities.action.js
+++ src/store/actions/charities.action.js
@@ -1,14 +1,19 @@
 import fetch from 'isomorphic-fetch';
-import { getErrorMessage } from './message.action';
+import { getErrorMessage, resetErrorMessage } from './message.action';
 
 export function getCharities() {
   return async (dispatch) => {
     try {
       const response = await fetch('http://localhost:3001/charities');
-      const data = await response.json();
-      dispatch({ type: 'TAMBOON/GET_CHARITIES', payload: data });
+      if (response.status >= 200 && response.status < 300) {
+        const data = await response.json();
+        dispatch({ type: 'TAMBOON/GET_CHARITIES', payload: data });
+      } else {
+        throw new Error('Cannot receive a charity data');
+      }
     } catch (e) {
       dispatch(getErrorMessage(e.message));
+      setTimeout(() => dispatch(resetErrorMessage()), 5000);
     }
   };
 }
--- src/store/actions/payments.action.js
+++ src/store/actions/payments.action.js
@@ -1,17 +1,44 @@
 import fetch from 'isomorphic-fetch';
-import { getErrorMessage } from './message.action';
+import { getErrorMessage, getSuccessMessage, resetSuccessMessage, resetErrorMessage } from './message.action';
+import { updateTotalDonate } from './donate.action';
 
 export function getPayments() {
   return async (dispatch) => {
     try {
       const response = await fetch('http://localhost:3001/payments');
-      const data = await response.json();
-      dispatch({
-        type: 'TAMBOON/GET_PAYMENTS',
-        payload: data,
-      });
+      if (response.status >= 200 && response.status < 300) {
+        const data = await response.json();
+        dispatch({
+          type: 'TAMBOON/GET_PAYMENTS',
+          payload: data,
+        });
+      } else {
+        throw new Error('Cannot receive a payment data');
+      }
     } catch (e) {
       dispatch(getErrorMessage(e.message));
+      setTimeout(() => dispatch(resetErrorMessage()), 5000);
+    }
+  };
+}
+
+export function makePayments(charitiesId, amount, currency) {
+  return async (dispatch, getState) => {
+    try {
+      const response = await fetch('http://localhost:3001/payments', {
+        method: 'POST',
+        body: JSON.stringify({ charitiesId, amount, currency }),
+      });
+      if (response.status >= 200 && response.status < 300) {
+        dispatch(getSuccessMessage(`Thanks for donate ${amount}!`));
+        dispatch(updateTotalDonate(amount + getState().donate));
+        setTimeout(() => dispatch(resetSuccessMessage()), 2000);
+      } else {
+        throw new Error('Cannot make a payment');
+      }
+    } catch (e) {
+      dispatch(getErrorMessage(e.message));
+      setTimeout(() => dispatch(resetErrorMessage()), 5000);
     }
   };
 }
